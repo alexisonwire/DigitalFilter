bb796dace0567ced25fe5b4aac5a538a3aac419c
LETSNOTE\user <adtd1989@gmail.com>
2021-08-29 02:59:59 +0900

    　

--- DigitalFilter.cs
+++ DigitalFilter.cs
@@ -8,12 +8,15 @@ namespace DigitalFilter
     /// </summary>
     public class DigitalFilter
     {
+        private double fa = 0.0f;
+        private double pfc = 0.0f;
         private double in1 = 0.0f;
         private double in2 = 0.0f;
         private double out1 = 0.0f;
         private double out2 = 0.0f;
         private Queue<double> buffer;
         public readonly double omega;
+        public readonly double [] z;
         public readonly double alpha;
         public readonly double internalBandWidth;
         public readonly double q;
@@ -35,20 +38,34 @@ namespace DigitalFilter
         /// <param name="bandWidth">帯域幅(Default = 1octave)</param>
         public DigitalFilter(double secControl, double cutoff, FilterType filterType, double bandWidth = 1.0f)
         {
+            z = new double[4];
             internalBandWidth = bandWidth;
             internalFilter = filterType;
             omega = 2.0f * PI * cutoff / secControl;
             q = 1.0f / Sqrt(2);
+            fa = 1.0 / (2.0 * PI) * Tan(PI * cutoff / secControl);
+            pfc = 2.0 * PI * fa;
+
             switch (internalFilter)
             {
                 case FilterType.LowPassFilter:
+                    //alpha = Sin(omega) / (2.0f * q);
+                    //a0 = 1.0f + alpha;
+                    //a1 = -2.0f * Cos(omega);
+                    //a2 = 1.0f - alpha;
+                    //b0 = (1.0f - Cos(omega)) / 2.0f;
+                    //b1 = 1.0f - Cos(omega);
+                    //b2 = (1.0f - Cos(omega)) / 2.0f;
+
                     alpha = Sin(omega) / (2.0f * q);
-                    a0 = 1.0f + alpha;
-                    a1 = -2.0f * Cos(omega);
-                    a2 = 1.0f - alpha;
-                    b0 = (1.0f - Cos(omega)) / 2.0f;
-                    b1 = 1.0f - Cos(omega);
-                    b2 = (1.0f - Cos(omega)) / 2.0f;
+                    b0 = 1.0f;
+                    b1 = (-2.0 + 2.0 * pfc * pfc) / (1 + Sqrt(2) * pfc + pfc * pfc);
+                    b2 = (1.0 - Sqrt(2) * pfc + pfc * pfc) / (1 + Sqrt(2) * pfc + pfc * pfc);
+
+                    a0 = pfc * pfc / (1 + Sqrt(2) * pfc + pfc * pfc);
+                    a1 = 2.0 * pfc * pfc / (1 + Sqrt(2) * pfc + pfc * pfc);
+                    a2 = pfc * pfc / (1 + Sqrt(2) * pfc + pfc * pfc);
+
                     break;
 
                 case FilterType.HighPassFilter:
@@ -106,6 +123,7 @@ namespace DigitalFilter
         /// <returns>フィルタ適用値</returns>
         public double FilterControl(double input)
         {
+            double output;
             switch (internalFilter)
             {
                 case FilterType.MovingAverageFilter:
@@ -121,8 +139,18 @@ namespace DigitalFilter
                     }
                     return (sum / averageNum);
 
+                case FilterType.LowPassFilter:
+                     output = input * a0 + z[0] * a1 + z[1] * a2
+                            - z[2] * b1 - z[3] * b2;
+                    z[1] = z[0];
+                    z[0] = input;
+                    z[3] = z[2];
+                    z[2] = output;
+
+                    return output;
+
                 default:
-                    double output = b0 / a0 * input +
+                    output = b0 / a0 * input +
                                             b1 / a0 * in1 +
                                             b2 / a0 * in2 -
                                             a1 / a0 * out1 -
@@ -131,7 +159,6 @@ namespace DigitalFilter
                     in1 = input;
                     out2 = out1;
                     out1 = output;
-
                     return output;
             }
         }
